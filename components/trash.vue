e19ad63a2be14f7e380706f18a2ca0b8f3a4a77c
meshkowich <lomonosow@gmail.com>
2019-08-09 17:58:59 +0300

    first

---
+++ components/trash.vue
@@ -0,0 +1,60 @@
+<template>
+  <div id="slide2" class="slide" style=" padding-top:61px; text-align:left;"><div class=" section_inner clearfix"><div class="section_inner_margin clearfix"><div class="wpb_column vc_column_container vc_col-sm-6"><div class="vc_column-inner "><div class="wpb_wrapper">
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			<h3>Education</h3>
+
+		</div> 
+	</div> <div class="vc_row wpb_row section vc_row-fluid vc_inner " style=" padding-top:56px; text-align:left;"><div class=" full_section_inner clearfix"><div class="wpb_column vc_column_container vc_col-sm-6 vc_col-has-fill"><div class="vc_column-inner vc_custom_1432547762965"><div class="wpb_wrapper"><div class="separator  transparent   " style="margin-top: 20px;margin-bottom: 0px;"></div>
+
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			<h4>Bachelor’s Degree</h4>
+
+		</div> 
+	</div> <div class="separator  transparent   " style="margin-top: 35px;margin-bottom: 0px;"></div>
+
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			<p>Vilnius University</p>
+<p>20010 – 2014</p>
+
+		</div> 
+	</div> <div class="separator  transparent   " style="margin-top: 20px;margin-bottom: 0px;"></div>
+
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			<p>Vilnius, Lithuania</p>
+<p>French Language &amp; Literature</p>
+
+		</div> 
+	</div> </div></div></div><div class="wpb_column vc_column_container vc_col-sm-6"><div class="vc_column-inner "><div class="wpb_wrapper"><div class="separator  transparent   " style="margin-top: 25px;margin-bottom: 0px;"></div>
+
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			<h4>Master’s Degree</h4>
+
+		</div> 
+	</div> <div class="separator  transparent   " style="margin-top: 25px;margin-bottom: 0px;"></div>
+
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			<p>California State University, Fullerton</p>
+<p>20015 – 2018</p>
+
+		</div> 
+	</div> <div class="separator  transparent   " style="margin-top: 25px;margin-bottom: 0px;"></div>
+
+	<div class="wpb_text_column wpb_content_element ">
+		<div class="wpb_wrapper">
+			<p>Fullerton, California</p>
+<p>Professional Communications</p>
+
+		</div> 
+	</div> </div></div></div></div></div></div></div></div><div class="wpb_column vc_column_container vc_col-sm-6"><div class="vc_column-inner "><div class="wpb_wrapper"><div class="q_progress_bar"><h5 class="progress_title_holder clearfix" style=""><span class="progress_title"><span>Adobe Creative Suite</span></span><span class="progress_number" style="font-size: 12px;font-weight: 600;"><span>65</span>%</span></h5><div class="progress_content_outer" style="height: 25px;"><div data-percentage="65" class="progress_content" style="height: 25px; background-color: rgb(218, 233, 239); border: 1px solid rgb(218, 233, 239); width: 65%;"></div></div></div><div class="q_progress_bar"><h5 class="progress_title_holder clearfix" style=""><span class="progress_title"><span>Eloqua</span></span><span class="progress_number" style="font-size: 12px;font-weight: 600;"><span>50</span>%</span></h5><div class="progress_content_outer" style="height: 25px;"><div data-percentage="50" class="progress_content" style="height: 25px; background-color: rgb(218, 233, 239); border: 1px solid rgb(218, 233, 239); width: 50%;"></div></div></div><div class="q_progress_bar"><h5 class="progress_title_holder clearfix" style=""><span class="progress_title"><span>WordPress</span></span><span class="progress_number" style="font-size: 12px;font-weight: 600;"><span>70</span>%</span></h5><div class="progress_content_outer" style="height: 25px;"><div data-percentage="70" class="progress_content" style="height: 25px; background-color: rgb(218, 233, 239); border: 1px solid rgb(218, 233, 239); width: 70%;"></div></div></div><div class="q_progress_bar"><h5 class="progress_title_holder clearfix" style=""><span class="progress_title"><span>Google Ads</span></span><span class="progress_number" style="font-size: 12px;font-weight: 600;"><span>85</span>%</span></h5><div class="progress_content_outer" style="height: 25px;"><div data-percentage="85" class="progress_content" style="height: 25px; background-color: rgb(218, 233, 239); border: 1px solid rgb(218, 233, 239); width: 85%;"></div></div></div><div class="q_progress_bar"><h5 class="progress_title_holder clearfix" style=""><span class="progress_title"><span>Google Analytics</span></span><span class="progress_number" style="font-size: 12px;font-weight: 600;"><span>85</span>%</span></h5><div class="progress_content_outer" style="height: 25px;"><div data-percentage="85" class="progress_content" style="height: 25px; background-color: rgb(218, 233, 239); border: 1px solid rgb(218, 233, 239); width: 85%;"></div></div></div></div></div></div></div></div></div>
+</template>
+<script>
+export default {
+	
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <!-- <div> 
+      <logo />
+      <h1 class="title">
+        nblog
+      </h1>
+      
+      <h2 class="subtitle">
+        front of nblog
+      </h2>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>-->
+    <parallax/>
+    <!-- <trash/> -->
+  </div>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+import Parallax from '~/components/parallax.vue'
+// import Trash from '~/components/trash.vue'
+
+export default {
+  components: {
+    Logo,
+    Parallax
+    // Trash
+  }  
+}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+}
+</style>
